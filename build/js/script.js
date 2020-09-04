9a33ec011d6c313ca375ecc69f2c52a3b5a70c48
Tunji Akanbi <you@example.com>
2020-09-04 12:26:53 -0500

    added javascript animations

--- build/js/script.js
+++ build/js/script.js
@@ -4,27 +4,53 @@ $(function () {
 
     let nav = document.querySelector('.site-nav');
     let meetMonsters = document.querySelector('#meet');
-    //let meetMonsters = $('#meet');
+    // $meetMonsters = $('#meet');
     // console.log(meetMonsters);
+    let monsterScroll = document.querySelectorAll('#monster-group .monster');
     let navHeight = nav.scrollHeight;
+
+    monsterScroll.forEach(
+        (item) => (item.style.animationDelay = `${Math.random() * 1 + 0.4}s`)
+    );
+
+    function inViewPort(el) {
+        let rect = el.getBoundingClientRect();
+        return (
+            (rect.top <= 0 && rect.bottom >= 0) ||
+            (rect.bottom >= window.innerHeight && rect.top <= window.innerHeight) ||
+            (rect.top >= 0 && rect.bottom <= window.innerHeight)
+        );
+    }
+
     function moveHeader() {
+        let top = window.pageYOffset;
         let mainOnTop = meetMonsters.getBoundingClientRect().top - navHeight;
-        mainOnTop < 0
-        ? nav.classList.add('in-body')
-        : nav.classList.remove('in-body')
-    //    if (mainOnTop < 0){
-    //        nav.classList.add('in-body')
-    //     } else {
-    //         nav.classList.remove('in-body')
-    // }
+        mainOnTop < 0 ?
+            nav.classList.add('in-body') :
+            nav.classList.remove('in-body')
+        //    if (mainOnTop < 0){
+        //        nav.classList.add('in-body')
+        //     } else {
+        //         nav.classList.remove('in-body')
+        // }
         //console.log(`${meetMonsters.getBoundingClientRect().top}`);
         //console.log(meetMonsters.getBoundingClientRect().top);
 
         let currentCuePosition = headerContent.getBoundingClientRect().top;
-        currentCuePosition < 0
-        ? headerCue.classList.add('d-none')
-        : headerCue.classList.remove('d-none')
-        
+        currentCuePosition < 0 ?
+            headerCue.classList.add('d-none') :
+            headerCue.classList.remove('d-none')
+
+        headerContent.style.transform = `translateY(-${top / 1.5}px)`;
+        headerContent.style.opacity = 1 - Math.max(top / (window.innerHeight * 0.2), 0);
+        // let test = 1 - Math.max(top/(window.innerHeight * 0.2), 0);
+        // console.log(test);
+
+        monsterScroll.forEach((item) =>
+            inViewPort(item) ?
+            item.classList.add('appear') :
+            item.classList.remove('appear')
+        )
         window.requestAnimationFrame(moveHeader);
     }
     window.requestAnimationFrame(moveHeader);
